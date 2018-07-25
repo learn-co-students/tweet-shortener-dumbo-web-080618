05fc40b509ed65e1491b9e8232a5b3c59c588400
Brennan Basinger <bdbasinger777@gmail.com>
2018-07-25 14:53:55 -0500

    Finished

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
-# Write your code here.
+
+
+def dictionary
+  name_of_this_is_irrelevant = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(str)
+  arr = str.split(' ')
+  result = arr.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  result.join(' ')
+end
+
+def bulk_tweet_shortener(arr_of_tweets)
+  arr_of_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    tweet[0...140]
+  else
+    word_substituter(tweet)
+  end
+end
+
+
+
+
+
+
+
+
